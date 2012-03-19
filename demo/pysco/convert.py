95467e20fba15ec0691f676a829527e21f6048ce
Jacob Joaquin/ <jacobjoaquin@gmail.com>
2012-03-18 18:16:58 -0700

    Added some MIDI, Conventional, and Hz value converters.

--- demo/pysco/convert.py
+++ demo/pysco/convert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import re
 
 def cpspch(p):
     octave, note = divmod(p, 1)
@@ -7,8 +8,26 @@ def cpspch(p):
 def dB(x):
 	return 10 ** (x / 20.0)
 
+def conv_to_hz(x):
+	return midi_to_hz(conv_to_midi(x))
 
+def conv_to_midi(note):
+	'''Conventional Notation to MIDI'''
 
+	accident = 0
+	values = dict(zip('CDEFGAB', [0, 2, 4, 5, 7, 9, 11]))
+	m = re.match(r"([A-G])(bb|b|##|#)?(\d+){1,2}", note)	
+	
+	if m.group(1) and m.group(3):
+		if m.group(2):
+			if m.group(2)[0] == 'b':
+				accident = len(m.group(2)) * -1
+			else:
+				accident = len(m.group(2))
 
+	return values[m.group(1)] + (int(m.group(3)) + 1) * 12 + accident
 
-
+def midi_to_hz(x):
+	'''MIDI to hz'''
+	
+	return 440 * 2 ** ((x - 69.0) / 12.0)
