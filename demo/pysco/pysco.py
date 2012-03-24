176e891d82042cb18a14cfaede8630c3d8be3471
Jacob Joaquin <jacobjoaquin@gmail.com>
2012-03-24 09:16:26 -0700

    Now supports simple timestacking.

--- demo/pysco/pysco.py
+++ demo/pysco/pysco.py
@@ -8,6 +8,7 @@ from random import random
 
 # Stores score output
 _sco = []
+exec_block = []
 time_stack = [0]
 
 
@@ -15,7 +16,6 @@ time_stack = [0]
 def debug_m(m, v=''):
 	print m + ': '
 	print v
-	print
 
 def i_event(*args):
 	global _sco
@@ -32,6 +32,7 @@ def pmap(statement, identifier, pfield, formula):
 	_sco = [csd.sco.map_("\n".join(_sco), {0: statement, 1: identifier}, pfield, formula)]
 
 def score(s):
+	debug_m('score(): ', s)
 	global _sco
 	global time_stack
 	#selected = sco.select_all(s)
@@ -43,28 +44,29 @@ def score(s):
 	s = sco.merge(s, foo)
 	_sco.append(s)
 
-def _parse():
-		exec_block = []
 
-		for line in f.readlines():
-			pfields = line.split()
+def _parse_timestack():
+	global f
+	global exec_block
 
-			if len(pfields) and len(pfields[0]) == 1:
-				print 'sco: ' + line
-				_sco.append(line)
+	debug_m('inside _parse_timestack')
 
-				if len(exec_block) >= 1:
-					print exec_block
-					exec("\n".join(exec_block))
-					exec_block = []
-			else:
-				print 'py:  ' + line
-				exec_block.append(line)
+	for line in f.readlines():
+		debug_m('line: ', line)
+		tokens = line.split()
 
-		if len(exec_block) >= 1:
-			print exec_block
+		if tokens and tokens[0][0] == '@':
+			debug_m('exec timestack', exec_block)
 			exec("\n".join(exec_block))
 			exec_block = []
+			time = tokens.pop(0)
+			time_stack.append(float(time[1:]))
+			exec(" ".join(tokens))
+			time_stack.pop()
+
+		else:
+			debug_m('exec_block', exec_block)
+			exec_block.append(line)
 
 
 # Begin
@@ -72,13 +74,20 @@ debug_m('--- Begin pysco ---')
 
 f = open(argv[1], 'r')
 
-foo = f.read()
-exec(foo)
+
+debug_m('pre _parse_timestack')
+_parse_timestack()
+debug_m('post _parse_timestack')
+
+if len(exec_block) >= 1:
+	debug_m('exec final block', exec_block)
+	exec("\n".join(exec_block))
+	exec_block = []
 
 f.close();
 
-debug_m('_sco', _sco)
 
+debug_m('final _sco:', _sco)
 # Output preprocessed score
 output = open(argv[2], 'w')
 output.write("\n".join(_sco))
---
+++ demo/pysco/test6.csd
@@ -0,0 +1,47 @@
+<CsoundSynthesizer>
+<CsInstruments>
+sr = 44100
+kr = 44100
+ksmps = 1
+nchnls = 1
+0dbfs = 1.0
+
+instr 1
+    idur = p3   ; Duration
+    iamp = p4   ; Amplitude
+    ifreq = p5  ; Frequency
+
+    kenv line iamp, idur, 0       ; Line envelope
+    a1 vco2 kenv, ifreq, 12, 0.5  ; Triangle wave
+    out a1
+endin
+
+</CsInstruments>
+<CsScore bin="./pysco.py">
+		
+def phrase():
+	score('''
+	i 1 0 0.5 -7 9.02
+	i 1 + .   .  8.07
+	i 1 + .   .  8.09
+	i 1 + .   .  8.11
+	i 1 + .   .  9.00
+	i 1 + .   .  8.09
+	i 1 + .   .  8.11
+	i 1 + .   .  8.07
+	''')
+
+score('''
+f 1 0 8192 10 1
+t 0 120
+''')
+
+#score('i 1 0 0.5 -7 9.02')
+
+@0 phrase()
+@4 phrase()
+
+pmap('i', 1, 4, dB)
+pmap('i', 1, 5, cpspch)
+</CsScore>
+</CsoundSynthesizer>
