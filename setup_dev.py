070c643bbf3ebe0a364e617bc11e750142833cb3
Jacob Joaquin <jacobjoaquin@gmail.com>
2015-04-17 16:19:21 -0700

    added setup_dev.py

---
+++ setup_dev.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(name='csd',
+      version='0.0.4',
+      description='Csound csd Python Package',
+      author='Jacob Joaquin',
+      author_email='jacobjoaquin@gmail.com',
+      url='http://jacobjoaquin.github.io/csd/pysco.html',
+      packages=['csd', 'csd.orc', 'csd.sco', 'csd.sco.element',
+                'csd.sco.event', 'csd.sco.selection', 'csd.pysco'],
+      )
+
